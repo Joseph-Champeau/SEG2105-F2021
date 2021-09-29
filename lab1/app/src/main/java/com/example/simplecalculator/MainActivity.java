e2a342ccd795a612f632818ff98771e4d62a06d2
Mohammad Alqahtani <Moohaaa531@gmail.com>
2021-09-29 16:19:45 +0700

    Working on interface, WIP

--- lab1/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ lab1/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,16 +1,10 @@
 package com.example.simplecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Button;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
