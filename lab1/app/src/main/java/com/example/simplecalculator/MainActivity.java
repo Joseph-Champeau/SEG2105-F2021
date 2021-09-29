6c2f1e526c4d2b5be98e66641aadfb443318dcdb
Mohammad Alqahtani <Moohaaa531@gmail.com>
2021-09-29 20:01:42 +0700

    Worked on java code

--- lab1/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ lab1/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnPlus, btnMinus, btnDiv,
+            btnMul, btnEqual, btnDec, btnClr;
+
+    TextView displayScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnMul = findViewById(R.id.btnMult);
+        btnEqual = findViewById(R.id.btnEqual);
+        btnDec = findViewById(R.id.btnDec);
+        btnDiv = findViewById(R.id.btnDiv);
     }
 }
