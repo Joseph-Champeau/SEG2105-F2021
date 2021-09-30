c2e6e4b0cbb1d612703c14ef0f410444f88fc1b5
Cdhaldane <48765995+Cdhaldane@users.noreply.github.com>
2021-09-30 00:27:33 -0400

    All math working

--- lab1/app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ lab1/app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -11,16 +11,12 @@ public class MainActivity extends AppCompatActivity {
     private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btnPlus, btnMinus, btnDiv,
             btnMul, btnEqual, btnDec, btnClr;
 
-
     TextView displayScreen ;
     TextView computationTextView;
-    private final char DIV = '/';
-    private final char MUL = '*';
-    private final char MINUS= '-';
-    private final char PLU = '+';
-    private char OPERATION;
-    private double val1= Double.NaN;
-    private double val2;
+
+    private enum Operator {none, add, minus, multiply, divide}
+    private double d1 = 0, d2 = 0;
+    private Operator op = Operator.none;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,64 +48,80 @@ public class MainActivity extends AppCompatActivity {
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "1");
+                displayScreen.setText(displayScreen.getText() +"1");
+                computationTextView.setText(computationTextView.getText() + "1");
             }
 
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "2");
+                displayScreen.setText(displayScreen.getText() +"2");
+                computationTextView.setText(computationTextView.getText() + "2");
             }
 
         });
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "3");
+                displayScreen.setText(displayScreen.getText() +"3");
+                computationTextView.setText(computationTextView.getText() + "3");
             }
 
         });
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "4");
+                displayScreen.setText(displayScreen.getText() +"4");
+                computationTextView.setText(computationTextView.getText() + "4");
             }
 
         });
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println(displayScreen);
                 displayScreen.setText(displayScreen.getText() + "5");
+                computationTextView.setText(computationTextView.getText() + "5");
             }
 
         });
         btn6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "6");
+                displayScreen.setText(displayScreen.getText() +"6");
+                computationTextView.setText(computationTextView.getText() + "6");
             }
 
         });
         btn7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "7");
+                displayScreen.setText(displayScreen.getText() +"7");
+                computationTextView.setText(computationTextView.getText() + "7");
             }
 
         });
         btn8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "8");
+                displayScreen.setText(displayScreen.getText() +"8");
+                computationTextView.setText(computationTextView.getText() + "8");
             }
 
         });
         btn9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "9");
+                displayScreen.setText(displayScreen.getText() +"9");
+                computationTextView.setText(computationTextView.getText() + "9");
+            }
+
+        });
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayScreen.setText(displayScreen.getText() +"0");
+                computationTextView.setText(computationTextView.getText() + "0");
             }
 
         });
@@ -118,10 +130,36 @@ public class MainActivity extends AppCompatActivity {
         btnEqual.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calculate();
-                displayScreen.setText(displayScreen.getText() + "9");
-                //displayScreen.setText(null);
+                if(op != Operator.none) {
 
+                    d2 = Double.parseDouble(displayScreen.getText().toString());
+                    double result;
+                    result = 0;
+                    if (op == Operator.add) {
+                        result = d1 + d2;
+                    } else if (op == Operator.minus) {
+                        result = d1 - d2;
+                    } else if (op == Operator.multiply) {
+                        result = d1 * d2;
+                    } else if (op == Operator.divide) {
+                        result = d1 / d2;
+                    }
+
+                    op = Operator.none;
+                    d1 = result;
+                    if ((result - (int) result) != 0) {
+                        result = Math.round(result * 1000d) / 1000d;
+                        displayScreen.setText(String.valueOf(result));
+                        computationTextView.setText(String.valueOf(result));
+                    }
+                    else {
+                        result = Math.round(result * 1000d) / 1000d;
+                        displayScreen.setText(String.valueOf((int) result));
+                        computationTextView.setText(String.valueOf(result));
+                    }
+
+
+                }
             }
 
             // +, -, *, need a similar to above but different approach. This is wrong.
@@ -129,28 +167,40 @@ public class MainActivity extends AppCompatActivity {
         btnMul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "*");
+                op = Operator.multiply;
+                d1 = Double.parseDouble(displayScreen.getText().toString());
+                displayScreen.setText("");
+                computationTextView.setText(computationTextView.getText() + "*");
             }
 
         });
         btnDiv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "/");
+                op = Operator.divide;
+                d1 = Double.parseDouble(displayScreen.getText().toString());
+                displayScreen.setText("");
+                computationTextView.setText(computationTextView.getText() + "/");
             }
 
         });
         btnPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "+");
+                op = Operator.add;
+                d1=Double.parseDouble(displayScreen.getText().toString());
+                displayScreen.setText("");
+                computationTextView.setText(computationTextView.getText() + "+");
             }
 
         });
         btnMinus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + "-");
+                op = Operator.minus;
+                d1 = Double.parseDouble(displayScreen.getText().toString());
+                computationTextView.setText(computationTextView.getText() + "-");
+                displayScreen.setText("");
             }
 
         });
@@ -158,7 +208,8 @@ public class MainActivity extends AppCompatActivity {
         btnDec.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                displayScreen.setText(displayScreen.getText() + ".");
+                displayScreen.setText(displayScreen.getText() +".");
+                computationTextView.setText(computationTextView.getText() + ".");
             }
 
         });
@@ -170,9 +221,10 @@ public class MainActivity extends AppCompatActivity {
                     CharSequence obj = displayScreen.getText().toString(); //assign the display screen as a character sequence
                     displayScreen.setText(obj.subSequence(0 ,obj.length()-1)); //upon click, it will remove the last value of the displayscreen
                 } else {
-                    val1=Double.NaN;
-                    val2=Double.NaN;
-                    displayScreen.setText(null);
+                    d1=Double.NaN;
+                    d2=Double.NaN;
+                    displayScreen.setText("");
+                    computationTextView.setText("");
 
                 }
             }
