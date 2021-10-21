6912a7251576d7ca9b5fb92031d5302e241ccf94
Michias Shiferaw <mshif073@uottawa.ca>
2021-10-21 15:06:29 -0400

    New Version!

---
+++ deliverable_1/app/src/main/java/com/example/deliverable_1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.deliverable_1;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button opengooglebtn;
+    private TextView teamAddress;
+
+    //TextView computationTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //opengooglebtn = (Button) findViewById(R.id.opengooglebtn);
+    }
+}
