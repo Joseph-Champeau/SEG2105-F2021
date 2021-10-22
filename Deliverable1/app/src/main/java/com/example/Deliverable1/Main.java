38a0075e6b56c8086ee9bd39fdf98c7de521b29f
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-10-21 22:30:54 -0400

    Added more

--- Deliverable1/app/src/main/java/com/example/Deliverable1/Main.java
+++ Deliverable1/app/src/main/java/com/example/Deliverable1/Main.java
@@ -1,15 +1,14 @@
 package com.example.Deliverable1;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -19,19 +18,21 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Main extends AppCompatActivity {
-
     private FirebaseUser user;
     private DatabaseReference reference;
     private String userID;
 
     private Button logout;
+    private  Button admin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        admin = (Button) findViewById(R.id.admin);
         logout = (Button) findViewById(R.id.signOut);
+
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -39,31 +40,61 @@ public class Main extends AppCompatActivity {
                 startActivity(new Intent(Main.this, FrontScreen.class));
             }
         });
-
         user = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("Users");
         userID = user.getUid();
 
         final TextView UsernameWTextView = (TextView) findViewById(R.id.UsernameW);
         final TextView TypeWTextView = (TextView) findViewById(R.id.TypeW);
+        final TextView EmailWTextView = (TextView) findViewById(R.id.emailW);
+        final TextView NameWTextView = (TextView) findViewById(R.id.nameW);
+        final TextView AgeWTextView = (TextView) findViewById(R.id.ageW);
 
         reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 User userProfile = snapshot.getValue(User.class);
-
                 if(userProfile != null){
                     String username = userProfile.username;
                     String type = userProfile.type;
+                    String email = userProfile.email;
+                    String age = userProfile.age;
+                    String name = userProfile.fullName;
 
-                    UsernameWTextView.setText("username: " + username);
-                    TypeWTextView.setText("type: " + type);
+                    UsernameWTextView.setText("Username: " + username);
+                    TypeWTextView.setText("Type: " + type);
+                    EmailWTextView.setText("Email: " + email);
+                    NameWTextView.setText("Name: "+ name);
+                    AgeWTextView.setText("Age: "+ age);
+
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(Main.this, "Something bad", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        admin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        User userProfile = snapshot.getValue(User.class);
+                        String type = userProfile.type;
+
+                        if(type.equals("admin")){
+                            startActivity(new Intent(Main.this, FrontScreen.class ));
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+                        Toast.makeText(Main.this, "Something bad", Toast.LENGTH_LONG).show();
+                    }
+                });
             }
         });
 
