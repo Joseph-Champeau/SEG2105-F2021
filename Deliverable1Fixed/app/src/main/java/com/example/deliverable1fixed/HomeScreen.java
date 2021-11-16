cb2d7cf0129f59db8def61012177e20f1f5d1120
Simon brunet <79413810+sbrun068@users.noreply.github.com>
2021-11-16 13:25:19 -0500

    Code cleanup, method docstrings added and other minor patches

--- Deliverable1Fixed/app/src/main/java/com/example/deliverable1fixed/HomeScreen.java
+++ Deliverable1Fixed/app/src/main/java/com/example/deliverable1fixed/HomeScreen.java
@@ -25,13 +25,13 @@ import com.google.firebase.database.ValueEventListener;
  *  @since 2021-11-17
  */
 public class HomeScreen extends AppCompatActivity {
+
     public String userID;
     private DatabaseReference reference;
     private Resources res;
 
-    private Button logout;
     private Button admin;
-    private Button userInstructorAdmin;
+    private Button instructor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,19 +41,18 @@ public class HomeScreen extends AppCompatActivity {
         res = getResources();
 
         admin = (Button) findViewById(R.id.adminButton); // viewed by admins only
-        userInstructorAdmin = (Button) findViewById(R.id.userInstructorMemberBtn); // viewed by all atm
-        logout = (Button) findViewById(R.id.signOut);
+        instructor = (Button) findViewById(R.id.instructorHomeBtn); // viewed by all users at the moment
+        Button logout = (Button) findViewById(R.id.signOut);
 
 
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(HomeScreen.this, FrontScreen.class));
             }
         });
 
-        userID = getIntent().getStringExtra("arg"); // passed from previous page
+        userID = getIntent().getStringExtra("arg"); // passed on from previous page
         reference = FirebaseDatabase.getInstance().getReference("Users");
 
         final TextView UsernameWTextView = (TextView) findViewById(R.id.UsernameW);
@@ -62,6 +61,7 @@ public class HomeScreen extends AppCompatActivity {
         final TextView NameWTextView = (TextView) findViewById(R.id.nameW);
         final TextView AgeWTextView = (TextView) findViewById(R.id.ageW);
 
+        // pulls user data from real time database
         reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -79,24 +79,28 @@ public class HomeScreen extends AppCompatActivity {
                     NameWTextView.setText("Name: "+ name);
                     AgeWTextView.setText("Age: "+ age);
                     if (userProfile.getType() != null) {
+                        instructor.setText(res.getString(R.string.instructor));
                         if (!(userProfile.getType().equals("Admin"))) { // if user is not Admin type
                             admin.setVisibility(View.GONE); // hide admin button from view
-                            if (userProfile.getType().equals("Instructor")) {
-                                userInstructorAdmin.setText(res.getString(R.string.instructor));
-                            }
                         }
-                        else {
-                            userInstructorAdmin.setText(res.getString(R.string.classes));
+                        /*
+                        if (userProfile.getType().equals("Instructor")) {
+                            // hide the member button from instructor view
+                        }
+                        */
+                        if (userProfile.getType().equals("Member")) {
+                            instructor.setVisibility(View.GONE); // hide instructor button from member view
                         }
                     }
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(HomeScreen.this, "Database Error", Toast.LENGTH_LONG).show();
             }
         });
+
+        // initiates the admin button protocol
         admin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -105,14 +109,12 @@ public class HomeScreen extends AppCompatActivity {
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
                         User userProfile = snapshot.getValue(User.class);
                         String type = userProfile.getType();
-
-                        if(type.equals("Admin")) {
+                        if(type != null && type.equals("Admin")) {
                             Intent intentClasses = new Intent(HomeScreen.this, AdminMain.class);
                             intentClasses.putExtra("arg", userID);
                             startActivity(intentClasses);
                         }
                     }
-
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
                         Toast.makeText(HomeScreen.this, "Database Error", Toast.LENGTH_LONG).show();
@@ -120,7 +122,9 @@ public class HomeScreen extends AppCompatActivity {
                 });
             }
         });
-        userInstructorAdmin.setOnClickListener(new View.OnClickListener() {
+
+        // initiates the instructor button's protocol
+        instructor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -129,18 +133,17 @@ public class HomeScreen extends AppCompatActivity {
                         User userProfile = snapshot.getValue(User.class);
                         String type = userProfile.getType();
 
-                        if(type.equals("Instructor")) {
+                        if(type != null && type.equals("Instructor")) {
                             Intent intentClasses = new Intent(HomeScreen.this, InstructorMain.class);
                             intentClasses.putExtra("arg", userID);
                             startActivity(intentClasses);
                         }
-                        if(type.equals("Admin") && userInstructorAdmin.getText().toString().equals("Classes")) {
+                        if(type != null && type.equals("Admin")) {
                             Intent intentClasses = new Intent(HomeScreen.this, InstructorMain.class);
                             intentClasses.putExtra("arg", userID);
                             startActivity(intentClasses);
                         }
                     }
-
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
                         Toast.makeText(HomeScreen.this, "Database Error", Toast.LENGTH_LONG).show();
