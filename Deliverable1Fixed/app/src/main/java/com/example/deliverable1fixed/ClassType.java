26763ef23ee88ca4e12c040a7ef4ea66bfb69e8c
Simon brunet <79413810+sbrun068@users.noreply.github.com>
2021-11-15 19:14:23 -0500

    Small patch

--- Deliverable1Fixed/app/src/main/java/com/example/deliverable1fixed/ClassType.java
+++ Deliverable1Fixed/app/src/main/java/com/example/deliverable1fixed/ClassType.java
@@ -5,16 +5,11 @@ public class ClassType {
     public String name;
     public String description;
 
+    public ClassType() {
+    }
+
     public ClassType(String name, String description) {
         this.name = name;
         this.description = description;
-    }
-
-    public String getClassTypeName() {
-        return this.name;
-    }
-
-    public String getClassTypeDesc() {
-        return this.description;
     }
 }
