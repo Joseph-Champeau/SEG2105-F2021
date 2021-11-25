81faa05b842a599f33bcc650b1275ebc48e3e1cd
Simon brunet <79413810+sbrun068@users.noreply.github.com>
2021-11-24 19:07:42 -0500

    Added JUnit test cases

--- Lab5/app/src/main/java/com/example/lab5junit/Calculator.java
+++ Lab5/app/src/main/java/com/example/lab5junit/Calculator.java
@@ -2,53 +2,71 @@ package com.example.lab5junit;
 
 import static java.lang.Math.pow;
 
-public class Calculator {    public double add(String op1, String op2) {
-    if (op1.equals("null")||op2.equals("null")){
-        return -1;
+public class Calculator {
 
+    public double add(String op1, String op2) {
+        if (op1.equals("null")||op2.equals("null")){
+            return -1;
+        } else {
+            return Double.parseDouble(op1) + Double.parseDouble(op2);
+        }
     }
-
-    return Double.parseDouble(op1) + Double.parseDouble(op2);
-}
 
     public double diff(String op1, String op2) {
         if (op1.equals("null")||op2.equals("null")){
             return -1;
-
+        } else {
+            return Double.parseDouble(op1) - Double.parseDouble(op2);
         }
-        return Double.parseDouble(op1) - Double.parseDouble(op2);
     }
 
     public double div(String op1, String op2) {
         if (op1.equals("null")||op2.equals("null")){
             return -1;
-
+        } else if (op2.equals("0")) {
+            return -1.1;
+        } else {
+            return Double.parseDouble(op1) / Double.parseDouble(op2);
         }
-        // if (op2 == 0) return 0;
-        return Double.parseDouble(op1) / Double.parseDouble(op2);
     }
 
-    public double expon(String op1, String op2){
+    public double mul(String op1, String op2) {
         if (op1.equals("null")||op2.equals("null")){
             return -1;
-
+        } else {
+            return Double.parseDouble(op1) * Double.parseDouble(op2);
         }
-        return pow(Double.parseDouble(op1), Double.parseDouble(op2));
+    }
+
+    public double expon(String op1, String op2) {
+        if (op1.equals("null")||op2.equals("null")) {
+            return -1;
+        } else if (op1.equals("0") && op2.equals("0")) {
+            return -1.1;
+        } else if (op1.equals("0") && Double.parseDouble(op2) < Double.parseDouble("0")) {
+            return -1.2;
+        } else {
+            return pow(Double.parseDouble(op1), Double.parseDouble(op2));
+        }
     }
 
     public double log10(String op1){
         if (op1.equals("null")){
             return -1;
-
+        } else if (Double.parseDouble(op1) < Double.parseDouble("0")) {
+            return -1.1;
+        } else if (op1.equals("0")) {
+            return -1.2;
+        } else {
+            return Math.log10(Double.parseDouble(op1));
         }
-        return Math.log10(Double.parseDouble(op1));
     }
+
     public double ceilingval(String op1){
         if (op1.equals("null")){
             return -1;
-
+        } else {
+            return Math.ceil(Double.parseDouble(op1));
         }
-        return Math.ceil(Double.parseDouble(op1));
     }
-
 }
--- Lab5/app/src/test/java/com/example/lab5junit/CalculatorTest.java
+++ Lab5/app/src/test/java/com/example/lab5junit/CalculatorTest.java
@@ -11,15 +11,16 @@ public class CalculatorTest {
 
     @BeforeClass
     public static void BeforeClass() {
+        // ???
         System.out.println("methods tested;\n testRover\n testGetStateFullName()");
-
     }
 
     @AfterClass
     public static void AfterClass() {
-        System.out.println(" Grade for Test (out of a possible 8 ): " + grade);
+        System.out.println(" Grade for Test (out of a possible 35.0 ): " + grade);
     }
-    @Test
+
+    @Test // weight: 7
     public void nullTest() {
         Calculator calc=new Calculator();
         String op1="null";
@@ -34,6 +35,9 @@ public class CalculatorTest {
         actual = calc.div(op1,op2);
         assertEquals(expected,actual,0.001);
         grade+=1;
+        actual = calc.mul(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
         actual = calc.expon(op1,op2);
         assertEquals(expected,actual,0.001);
         grade+=1;
@@ -43,11 +47,12 @@ public class CalculatorTest {
         actual = calc.ceilingval(op1);
         assertEquals(expected,actual,0.001);
         grade+=1;
-
     }
-    @Test
+
+    @Test // weight: 3
     public void addTest() {
         Calculator calc=new Calculator();
+
         String op1="3";
         String op2 ="5";
         double expected=8;
@@ -55,21 +60,231 @@ public class CalculatorTest {
         assertEquals(expected,actual,0.001);
         grade+=1;
 
-    }
+        op1="-123";
+        op2 ="-531";
+        expected=-654;
+        actual = calc.add(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
 
-    @Test
-    public void addTest2() {
-        Calculator calc=new Calculator();
-        String op1="-123";
-        String op2 ="-531";
-        double expected=-654;
-        double actual = calc.add(op1,op2);
+        op1="123";
+        op2 ="-531";
+        expected=-408;
+        actual = calc.add(op1,op2);
         assertEquals(expected,actual,0.001);
         grade+=1;
     }
 
-    /*-----------------------------------Add more test methods for the rest of the Calculator.java Class--------------------------------------*/
+    @Test // weight: 4
+    public void diffTest() {
+        Calculator calc=new Calculator();
 
+        String op1="20";
+        String op2 ="14";
+        double expected=6;
+        double actual = calc.diff(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="-20";
+        op2 ="5";
+        expected=-25;
+        actual = calc.diff(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="20";
+        op2 ="-5";
+        expected=25;
+        actual = calc.diff(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="-25";
+        op2 ="-5";
+        expected=-20;
+        actual = calc.diff(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+    }
+
+    @Test // weight: 4
+    public void divTest() {
+        Calculator calc=new Calculator();
+
+        String op1="100";
+        String op2 ="100";
+        double expected=1;
+        double actual = calc.div(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="100";
+        op2 ="-100";
+        expected=-1;
+        actual = calc.div(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="0";
+        op2 ="-100";
+        expected=0;
+        actual = calc.div(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="-100";
+        op2 ="-100";
+        expected=1;
+        actual = calc.div(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+    }
+
+    @Test // weight: 4
+    public void mulTest() {
+        Calculator calc=new Calculator();
+
+        String op1="2";
+        String op2 ="100";
+        double expected=200;
+        double actual = calc.mul(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="0";
+        op2 ="100";
+        expected=0;
+        actual = calc.mul(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="-100";
+        op2 ="-1";
+        expected=100;
+        actual = calc.mul(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="-100";
+        op2 ="1";
+        expected=-100;
+        actual = calc.mul(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+    }
+
+    @Test // weight: 4
+    public void exponTest() {
+        Calculator calc=new Calculator();
+
+        String op1="100";
+        String op2 ="3";
+        double expected=1000000;
+        double actual = calc.expon(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="100";
+        op2 ="-3";
+        expected=0.000001;
+        actual = calc.expon(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="-100";
+        op2 ="-2";
+        expected=0.0001;
+        actual = calc.expon(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="100";
+        op2 ="0";
+        expected=1;
+        actual = calc.expon(op1,op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+    }
+
+    @Test // weight: 2
+    public void log10Test() {
+        Calculator calc=new Calculator();
+
+        String op1="100";
+        double expected=2;
+        double actual = calc.log10(op1);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="1";
+        expected=0;
+        actual = calc.log10(op1);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+    }
+
+    @Test // weight: 2
+    public void ceilingvalTest() {
+        Calculator calc=new Calculator();
+
+        String op1="0.7";
+        double expected=1;
+        double actual = calc.ceilingval(op1);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        op1="-0.12";
+        expected=0;
+        actual = calc.ceilingval(op1);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+    }
+
+    @Test // weight: 5
+    public void illegalCalculationErrorTest() {
+        Calculator calc=new Calculator();
+        // Division by 0
+        String op1="1";
+        String op2="0";
+        double expected=-1.1;
+        double actual = calc.div(op1, op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        // Exponentiation where op1 && op2 == 0
+        op1="0";
+        op2="0";
+        expected=-1.1;
+        actual = calc.expon(op1, op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        // Exponentiation where either op1 == 0 && op2 < 0
+        op1="0";
+        op2="-8";
+        expected=-1.2;
+        actual = calc.expon(op1, op2);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        // Log10 where either op1 < 0
+        op1="-1";
+        expected=-1.1;
+        actual = calc.log10(op1);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+
+        // Log10 where either op1 == 0
+        op1="0";
+        expected=-1.2;
+        actual = calc.log10(op1);
+        assertEquals(expected,actual,0.001);
+        grade+=1;
+    }
+
+
+    /*-----------------------------------Add more test methods for the rest of the Calculator.java Class--------------------------------------*/
 
 
 }
